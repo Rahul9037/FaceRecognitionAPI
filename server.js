1069a80ae065246037f43e4313878569dc9be609
Vipin GS <vipings@aol.in>
2020-07-29 11:44:12 +0530

    changed connection string

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'rahulsmac',
-        password: '',
-        database: 'smart-brain'
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
@@ -24,19 +24,15 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get("/", (req, res) => {
-    res.json(database.users);
-})
+app.post("/signin", (req, res) => { signin.handleSignIn(req, res, db, bcrypt) })
 
-app.post("/signin", (req,res) => {signin.handleSignIn(req,res,db,bcrypt)})
+app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
-app.post("/register", (req,res) => {register.handleRegister(req,res,db,bcrypt)})
+app.get("/profile/:id", (req, res) => { profile.handleProfile(req, res, db) })
 
-app.get("/profile/:id", (req,res) => {profile.handleProfile(req,res,db)})
+app.put("/image", (req, res) => { image.handleImage(req, res, db) })
 
-app.put("/image", (req,res) => {image.handleImage(req,res,db)})
-
-app.put("/imageUrl", (req,res) => {image.handleImageUrl(req,res)})
+app.put("/imageUrl", (req, res) => { image.handleImageUrl(req, res) })
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`its working fine on ${process.env.PORT}!!!`);
